0cd259304e61fa8e497962b29cac0bc4672a18d0
Alexander Shogenov <i@shagg.ru>
2020-11-10 11:43:26 +0300

    Fix export default

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,2 +1,12 @@
-import PgRedis from './PgRedis';
+interface PgRedisOptions {
+    prefix?: string;
+    expire?: number;
+}
+declare class PgRedis {
+    private pg;
+    private redis;
+    private options;
+    constructor(pg: any, redis: any, options: PgRedisOptions);
+    query(sql: string, params?: any[]): Promise<any>;
+}
 export default PgRedis;
--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var PgRedis_1 = require("./PgRedis");
-exports["default"] = PgRedis_1["default"];
